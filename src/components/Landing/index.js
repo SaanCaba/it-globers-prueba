163158fc587126f8dc411e7ce80e874eeaa522d2
Santiago Caballero <saantyc12.caballero@gmail.com>
2023-03-15 09:48:04 -0300

    change background image

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -12,6 +12,12 @@ const Landing = () => {
 			<div className='contBtnLanding'>
 				<button>SHOP</button>
 			</div>
+			<img
+				className='imgLanding'
+				src={
+					'https://www.happers.es/server/Portal_0010689/img/galeria/carrusel_happers_to_go_s7_30455.jpg'
+				}
+			/>
 		</section>
 	);
 };
